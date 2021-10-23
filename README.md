4625ef0351a8d2cb46151811c54e5f7dbeeb2737
jeremy boissevain <jbois001@odu.edu>
2021-10-22 23:24:25 -0400

    Added .gitignore, README, and gradle project starting files. Let me know if i've committed something I shouldn't have

---
+++ README.md
@@ -0,0 +1,1 @@
+Design Document: https://docs.google.com/document/d/1CF0UkPwFXbDLoz3yyrVbw0558vdLQK_K5KOYjshU2GU/edit?usp=sharing
---
+++ app/src/main/java/DupDetector.java
@@ -0,0 +1,5 @@
+public class DupDetector {
+    public static void main(String [] args) {
+        System.out.println("This is placeholder");
+    }
+}
