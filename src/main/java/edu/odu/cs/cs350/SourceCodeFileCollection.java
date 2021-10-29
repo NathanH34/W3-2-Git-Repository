b9b57d3bc19394f6ab1085a7fd8d1e2cbc48f851
qnguy011 <qnguy011@odu.edu>
2021-10-29 19:05:49 -0400

    stubbed SourceCodeFIleCollection

---
+++ src/main/java/edu/odu/cs/cs350/SourceCodeFileCollection.java
@@ -0,0 +1,16 @@
+public class SourceCodeFileCollection {
+    private List<SourceCodeFile> files;
+
+    public void add(SourceCodeFile newFile){
+        files.add(newFile);
+    }
+
+    public void order(){
+
+    }
+
+    public void output(){
+
+    }
+
+}
