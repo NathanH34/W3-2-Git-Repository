1083102c2d4792a1951ed9ae08539038f325edf9
NathanH34 <nhoeg001@odu.edu>
2021-10-31 12:52:30 -0400

    Added SourceCodeFileCollection Iterator

--- src/main/java/edu/odu/cs/cs350/SourceCodeFileCollection.java
+++ src/main/java/edu/odu/cs/cs350/SourceCodeFileCollection.java
@@ -1,8 +1,9 @@
 package edu.odu.cs.cs350;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
-public class SourceCodeFileCollection {
+public class SourceCodeFileCollection implements Iterable<SourceCodeFile> {
     private ArrayList<SourceCodeFile> files;
 
     public SourceCodeFileCollection() {
@@ -22,13 +23,27 @@ public class SourceCodeFileCollection {
         Collections.sort(files);
     }
 
+    /**
+     *
+     */
     public SourceCodeFile getFile(int i) {
         return files.get(i);
     }
 
-    public void output(){
+    /**
+     * Iterable implementation
+     */
+    @Override
+    public final Iterator<SourceCodeFile> iterator() {
+        return files.iterator();
+    }
+
+    @Override
+    public final String toString() {
+        StringBuilder fileCollection = new StringBuilder();
         for (SourceCodeFile f: files) {
-            System.out.print(f.toString());
+            fileCollection.append(f.toString());
         }
+        return fileCollection.toString();
     }
 }
