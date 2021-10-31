8bf166b5fce35dc26e7a2ded6ea72dda21dcae55
wvarnier <wvarn001@odu.edu>
2021-10-31 10:58:31 -0400

    fixed searchFiles function and added code to go through all path arguments

--- src/main/java/edu/odu/cs/cs350/DupDetector.java
+++ src/main/java/edu/odu/cs/cs350/DupDetector.java
@@ -18,39 +18,37 @@ public class DupDetector {
             System.out.print(e.toString());
         }
         try {
-            File file = new File(args[1]);
-            SourceCodeFile src = new SourceCodeFile(file.getAbsolutePath());
-            System.out.print(src.toString());
-            Iterator<Token> it =  src.iterator();
-            while(it.hasNext()) {
-                System.out.print(it.next().toString());
-            }
+            for(int i=1; i<args.length; i++) { //go through all specified file paths
+            	File file = new File(args[i]);
+            	if(file.isFile()) { //check if path argument represents a file
+	            	SourceCodeFile src = new SourceCodeFile(file.getAbsolutePath());
+	            	System.out.print(src.toString());
+	                Iterator<Token> it =  src.iterator();
+	                while(it.hasNext()) {
+	                    System.out.print(it.next().toString());
+	                }
+            	}
+            	else if(file.isDirectory()) { //check if path argument represents a directory
+            		searchFiles(file);
+            	}
+            }      
         } catch(Exception e) {
             System.out.print(e.toString());
         }
-        
-       
+           
     }
     
     /**
-     * Recursively search for files in a directory and its subdirectories
-     * @param file directory to be searched
+     * Recursively search for files in a directory and its sub-directories and print their absolute paths
+     * @param dir directory to be searched
      */
-    public static void searchFiles(File file) {
-    	if(file.isFile()) { //check if path argument represents a file
-        	SourceCodeFile src = new SourceCodeFile(file.getAbsolutePath());
-            System.out.print(src.toString());
-        }
-    	else if(file.isDirectory()) { //check if path argument represents a directory
-    		File[] files = file.listFiles();
-    		for(File f: files) {
-    			System.out.println(f.getAbsolutePath());
-    			if(f.isDirectory()) { //If there is a sub-directory
-    				searchFiles(f);   //search it
-    			}
-                else if(f.isFile()) { //If there is a file in the directory
-                    searchFiles(f);   //**Should we confirm that file is .cpp or .h?
-                }
+    public static void searchFiles(File dir) {
+    	File[] files = dir.listFiles();
+    	for(File f: files) {
+    		SourceCodeFile srcFile = new SourceCodeFile(f.getAbsolutePath());
+    		System.out.print(srcFile.toString());
+    		if(f.isDirectory()) {
+    			searchFiles(f);
     		}
     	}
     }
