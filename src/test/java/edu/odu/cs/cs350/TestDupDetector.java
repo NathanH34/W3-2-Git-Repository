a428fe3432c2315355ce4e7dd0b26e7eb93bd15c
Vrand001 <92219865+Vrand001@users.noreply.github.com>
2021-10-31 19:01:14 -0400

    Update TestDupDetector.java

--- src/test/java/edu/odu/cs/cs350/TestDupDetector.java
+++ src/test/java/edu/odu/cs/cs350/TestDupDetector.java
@@ -4,10 +4,10 @@ import java.io.File;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
+
 
 public class TestDupDetector {
 
