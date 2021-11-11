950e56c2c29830e9e54beb0d5ae646a918446c99
ddean007 <91852407+ddean007@users.noreply.github.com>
2021-11-11 16:03:06 -0500

    Added imports to InputProperties class and test class files

---
+++ src/main/java/edu/odu/cs/cs350/InputProperties.java
@@ -0,0 +1,7 @@
+package edu.odu.cs.cs350;
+
+public class InputProperties {
+    /*
+    * TODO: Specify the filepath to a properties file in the command line arguments to be read from.
+    */
+}
---
+++ src/test/java/edu/odu/cs/cs350/TestInputProperties.java
@@ -0,0 +1,12 @@
+package edu.odu.cs.cs350;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+public class TestInputProperties {
+    
+}
