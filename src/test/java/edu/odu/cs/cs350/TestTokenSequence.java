6b49bca2b7684284373a18273dbb05131b4820eb
wvarnier <wvarn001@odu.edu>
2021-12-05 10:15:20 -0500

    added testToString function to TestTokenSequence

--- src/test/java/edu/odu/cs/cs350/TestTokenSequence.java
+++ src/test/java/edu/odu/cs/cs350/TestTokenSequence.java
@@ -10,9 +10,13 @@ import static org.hamcrest.Matchers.*;
 
 public class TestTokenSequence {
 	TokenSequence blankTokenSequence = new TokenSequence();
-    String defaultFilePath = "src/test/data/TestFile1.cpp";
+    String defaultFilePath1 = "src/test/data/TestFile1.cpp";
+    String defaultFilePath2 = "src/test/data/TestFile2.cpp";
+    String defaultFilePath3 = "src/test/data/TestFile3.cpp";
+    String defaultFilePath4 = "src/test/data/TestFile4.cpp";
     int sequenceLength = 8;
     int sequenceStartLocation = 0;
+    LinkedHashMap<Lexeme, Integer> blankLexMap;
     ArrayList<Integer> blankParameterOrder;
 
     /**
@@ -26,12 +30,20 @@ public class TestTokenSequence {
     @Test
     public void testConstructor() {
         TokenSequence tokenSequence1 = new TokenSequence();
-        assertThat(tokenSequence1, equalTo(blankTokenSequence));
+        assertEquals(tokenSequence1.getLexemeMap(), blankTokenSequence.getLexemeMap());
+        assertEquals(tokenSequence1.getParameterOrder(), blankTokenSequence.getParameterOrder());
+        assertEquals(tokenSequence1.getTokens(), blankTokenSequence.getTokens());
+        
+        ArrayList<CPPToken> cppTokens = new ArrayList<CPPToken>();
+        CPPToken cppToken = new CPPToken(TokenKinds.INTEGER_LITERAL, 1, 1);
+        cppTokens.add(cppToken);
+        tokenSequence1.setTokens(cppTokens);
+        assertNotEquals(tokenSequence1.getTokens(), blankTokenSequence.getTokens());
     }
 
     @Test
     public void testConstructorWithParam() {
-        SourceCodeFile sourceCodeFile1 = new SourceCodeFile(defaultFilePath);
+        SourceCodeFile sourceCodeFile1 = new SourceCodeFile(defaultFilePath1);
         ArrayList<CPPToken> subSetTokenSequence = 
             sourceCodeFile1.makeTokenSequence(sequenceLength, sequenceStartLocation);
         
@@ -41,5 +53,19 @@ public class TestTokenSequence {
         assertThat(tokenSequence1.getSourceCode(), equalTo(sourceCodeFile1));
         assertThat(tokenSequence1.getParameterOrder(), not(equalTo(blankParameterOrder)));
         //lexemeMap and tokens check
+    }
+       
+    @Test
+    public void testToString() {
+    	SourceCodeFile testFile1 = new SourceCodeFile(defaultFilePath3);
+    	TokenSequence tokenSequence1 = new TokenSequence(testFile1.getTokens(), testFile1, sequenceStartLocation);
+    	String expected1 = "x y 1 cout \n";
+    	assertEquals(tokenSequence1.toString(), expected1);
+    	
+    	SourceCodeFile testFile2 = new SourceCodeFile(defaultFilePath2);
+    	TokenSequence tokenSequence2 = new TokenSequence(testFile2.getTokens(), testFile2, sequenceStartLocation);
+    	String expected2 = "iostream \n";
+    	assertNotEquals(tokenSequence2.toString(), expected1);
+    	assertEquals(tokenSequence2.toString(), expected2);
     }
 }
